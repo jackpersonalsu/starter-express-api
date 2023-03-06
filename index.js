77e4a301e337c2f03a32a95530688c6218b2b1af
Shirley Su <shirley2452452453@gmail.com>
2023-03-05 21:46:32 -0800

    adding

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo from jack!')
+    res.send('aaYo from jack!')
 })
 app.listen(process.env.PORT || 3000)
